7568971c48dfc8e94a13cffffd7d51d21f309b13
June Dev <55070877+khj951213@users.noreply.github.com>
2024-02-26 22:56:12 +1100

    Update

---
+++ JuneDev-Mailkit-Example/JuneDev-Mailkit-Example/MailHandler.cs
@@ -0,0 +1,89 @@
+﻿using MimeKit;
+using System.Net.Mail;
+using System.Text.Json;
+using MailKit.Net.Smtp;
+
+namespace JuneDev_Mailkit_Example
+{
+    public class MailHandler
+    {
+        private string? _email;
+        private string? _password;
+        private string? _server;
+        private int? _port;
+        private string? _name;
+
+        public MailHandler()
+        {
+
+        }
+
+        public MailHandler(string email, string password, string server, int port, string name)
+        {
+            _email = email;
+            _password = password;
+            _server = server;
+            _port = port;
+            _name = name;
+        }
+
+        public string? Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        public string? Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+        public string? Server
+        {
+            get { return _server; }
+            set { _server = value; }
+        }
+
+        public int? Port
+        {
+            get { return _port; }
+            set { _port = value; }
+        }
+
+        public string? Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public async Task<bool> SendEmail(string subject, string content, string recvEmail, string? recvName)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(_name, _email));
+            message.To.Add(new MailboxAddress(recvName, recvEmail));
+            message.Subject = subject;
+            message.Body = new TextPart("plain")
+            {
+                Text = content
+            };
+
+            using (var client = new MailKit.Net.Smtp.SmtpClient())
+            {
+                client.Connect(_server, _port ?? 587, false); // if use ssl true; otherwise, false
+                client.AuthenticationMechanisms.Remove("XOAUTH2"); // Disable OOAUTH2
+                client.Authenticate(_email, _password); // authenticate with the server (throws exception if failed)
+                await client.SendAsync(message); // send email
+                client.Disconnect(true); // disconnect from server
+            }
+
+            return true;
+        }
+
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
---
+++ JuneDev-Mailkit-Example/JuneDev-Mailkit-Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using JuneDev_Mailkit_Example;
+
+var mailHandler = new MailHandler()
+{
+    Email = "your_email_address",
+    Password = "your_email_password",
+    Server = "your_email_server",
+    Port = 587, // your_email_port_number
+    Name = "your_name"
+};
+
+await mailHandler.SendEmail("Email from June", "Test email", "khj951213@gmail.com", "Hyun June Kim");
